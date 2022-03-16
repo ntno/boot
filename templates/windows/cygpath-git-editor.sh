fbf65e46236db9f9e4b57f8c7c6180354ad6a521
ntno <ntno@users.noreply.github.com>
2022-03-16 07:26:43 -0700

    improved commentary

--- templates/windows/cygpath-git-editor.sh
+++ templates/windows/cygpath-git-editor.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
+# this script helps cygwin to properly use vscode as an external editor for git
+# save this file to your home directory and make executable (chmod +x)
+# then set core.editor git option to the path to this file
+# example:
+# editor="/home/this/file.sh" in .gitconfig
+#
 # author nickbudi
 # https://gist.github.com/nickbudi/4b489f50086db805ea0f3864aa93a9f8
-# install instructions: Daniel Kaplan
-# https://stackoverflow.com/questions/62724723/git-in-visual-studio-code-says-file-is-modified-even-when-there-is-no-change/69340165#69340165
 
-
-# wrapper to convert linux paths to windows
-# so vscode will work as a git editor with cygwin
-# editor="/home/this/file.sh" in .gitconfig
 
 # extract last argument (the file path)
 for last; do true; done
